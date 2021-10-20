4cec6d35ab4118210ce6596f3e9cf601338175c9
Muhammad Lehrmann <osheanman@berkeley.edu>
2021-10-20 21:49:37 +0000

    part 1 complete now

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
     @all_ratings = Movie.all_ratings
-    @ratings_to_show = {}
+    if(params.has_key?(:ratings)
+      @ratings_to_show = params(:ratings).stringify_keys
+      else
+      @ratings_to_show = {}
+      end
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
