729c542f54980cc852b56a1a9d0dcc4c306828d2
Muhammad Lehrmann <osheanman@berkeley.edu>
2021-10-20 21:39:30 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil?
+      return self.all
+    else
+      return self.where(rating: ratings_list)
+    end
+  end
+  
+  def self.all_ratings()
+    return ['G','PG','PG-13','R']
+  end
 end
