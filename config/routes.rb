cbff73471813d316a747a4d3528536f50a2bfe12
Muhammad Lehrmann <osheanman@berkeley.edu>
2021-10-21 07:35:32 +0000

    plzplzplz234567

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  
+  match '/movies/:sort_by' => 'movies#index', :as => :movies
+
 
   resources :movies
   
